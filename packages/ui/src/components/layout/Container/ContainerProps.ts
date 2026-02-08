c111fab55031dc6536931de4fe9a5e64c644af83
Alex <yuto.oku21@gmail.com>
2026-02-09 02:52:00 +0900

    不要なrecipeを削除

--- packages/ui/src/components/layout/Container/ContainerProps.ts
+++ packages/ui/src/components/layout/Container/ContainerProps.ts
@@ -1,6 +1,3 @@
 import type { HTMLAttributes } from 'react';
-import { containerRecipe } from './Container.css';
-import type { RecipeVariants } from '@vanilla-extract/recipes';
 
-export type ContainerProps = HTMLAttributes<HTMLDivElement> &
-  RecipeVariants<typeof containerRecipe>;
+export type ContainerProps = HTMLAttributes<HTMLDivElement>;
